53e38969ffe1200adf04b2639cf5b044c753f3a7
김대웅 <dkssud9556@dsm.hs.kr>
2021-03-25 14:23:36 +0900

    Add typeorm config in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from './config';
+import { UserModule } from './user/user.module';
+import { ReportModule } from './report/report.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: config.database.host,
+      port: config.database.port,
+      username: config.database.username,
+      password: config.database.password,
+      database: config.database.database,
+      entities: config.database.entities,
+      synchronize: config.database.synchronize,
+    }),
+    UserModule,
+    ReportModule,
+    ChatModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
