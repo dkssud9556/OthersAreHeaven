ee66ece835ab83a783bedf928dec4473634b6a6a
김대웅 <dkssud9556@dsm.hs.kr>
2021-03-26 09:53:22 +0900

    Delete useless files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import config from './config';
 import { UserModule } from './user/user.module';
@@ -25,7 +23,5 @@ import { AuthModule } from './auth/auth.module';
     ChatModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
