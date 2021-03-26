9629bce284a8753a7b711beb6927fa1992557613
김대웅 <dkssud9556@dsm.hs.kr>
2021-03-26 09:55:14 +0900

    Add login strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './service/auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from '../user/user.module';
+import { AuthController } from './controller/auth.controller';
+import { PasswordService } from './service/password.service';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: jwtConstants.expiresIn },
+    }),
+    UserModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, PasswordService],
 })
 export class AuthModule {}
