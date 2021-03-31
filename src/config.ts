a8bd185a375467d96795f1e65c07f29cf594cffd
김대웅 <dkssud9556@dsm.hs.kr>
2021-03-31 16:49:54 +0900

    Fix database config

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,4 @@
 import { config } from 'dotenv';
-import { UserEntity } from './user/domain/user.entity';
-import { RoomEntity } from './user/domain/room.entity';
-import { ReportEntity } from './report/domain/report.entity';
-import { ChatEntity } from './chat/domain/chat.entity';
 
 if (process.env.NODE_ENV !== 'production') {
   config();
@@ -10,13 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 export default {
   database: {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    entities: [UserEntity, RoomEntity, ReportEntity, ChatEntity],
-    synchronize: process.env.NODE_ENV !== 'production',
+    url: process.env.DB_URL,
   },
   jwt: {
     secret: process.env.JWT_SECRET,
