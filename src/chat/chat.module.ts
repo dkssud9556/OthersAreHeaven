521cba6d9acc08923a8253fa714f9d1cfae29d56
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-28 11:16:30 +0900

    [UPDATE] <ChatModule>

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Chat, ChatSchema } from './schema/chat.schema';
 import { ChatGateway } from './chat.gateway';
+import { AuthModule } from '../auth/auth.module';
+import { AuthGuard } from './auth.guard';
+import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
+    AuthModule,
+    UserModule,
   ],
-  providers: [ChatGateway],
+  providers: [ChatGateway, AuthGuard],
 })
 export class ChatModule {}
